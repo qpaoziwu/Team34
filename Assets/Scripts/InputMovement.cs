a3e273c5ef895edb16ca74c04b139b51018cff9c
Richard <richrdhw@gmail.com>
2020-03-02 23:21:07 -0500

    Updated Animation and Aim Mode

--- Assets/Scripts/InputMovement.cs
+++ Assets/Scripts/InputMovement.cs
@@ -5,6 +5,7 @@ public class InputMovement : MonoBehaviour
 {
     //Lerping Movement Speed
     public float speed;
+    public float slowedSpeed;
     public float percent;
     public float changeSpeedInterval;
     private float startTimer = 0;
@@ -20,12 +21,13 @@ public class InputMovement : MonoBehaviour
     public float jumpForce;
     public float dropElapsed;
     public bool isGrounded;
+    public bool isAiming;
 
     public int playerLayer;
     public int groundLayer;
     public Transform lineCastStart;
     public Transform lineCastEnd;
-    public BoxCollider2D box;
+    //public BoxCollider2D box;
     Animator animatorComponent;
     SpriteRenderer spriteRendererComponent;
 
@@ -37,11 +39,9 @@ public class InputMovement : MonoBehaviour
         // Get references to components
         animatorComponent = GetComponent<Animator>();
         spriteRendererComponent = GetComponent<SpriteRenderer>();
-        box = GetComponent<BoxCollider2D>();
+
         playerLayer = LayerMask.GetMask("Player");
         groundLayer = LayerMask.GetMask("Ground");
-
-
         isFacingRight = true;
     }
 
@@ -52,46 +52,121 @@ public class InputMovement : MonoBehaviour
         speed = 0;
         // reset the timer
         ResetTimer();
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(lineCastStart.position, lineCastEnd.position, Color.green);
+
+        // Check if grounded
+        isGrounded = (Physics2D.Linecast(lineCastStart.position, lineCastEnd.position, groundLayer)) ? true : false;
+        isAiming = Input.GetKey(KeyCode.J);
+
+        DropDownCheck();
+
+
+
+
+        InputHandler();
+        // Sprite Animation Parameters
+        AnimateSprite();
+
 
     }
+
     void FixedUpdate()
     {
         // move toward the target position using the interpolated speed
         transform.position = Vector2.MoveTowards(transform.position, transform.position + Vector3.right * horizontalInput, LerpSpeed() * Time.deltaTime);
 
     }
-    // Update is called once per frame
-    void Update()
+
+    void InputHandler()
     {
-        DropDownCheck();
-        Debug.DrawLine(lineCastStart.position, lineCastEnd.position, Color.green);
-
-        // Check if grounded
-        isGrounded = (Physics2D.Linecast(lineCastStart.position, lineCastEnd.position, groundLayer)) ? true : false;
-
-
-        // Input
         horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (isAiming)
         {
-            //ShootHook
-        }
-        //ResetTimer(dropTimer);
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!dropping)
+            speed = Mathf.Clamp(horizontalInput, slowedSpeed, slowedSpeed);
+            if (Input.GetKeyDown(KeyCode.K) && isGrounded)
             {
-                //ResetTimer(dropTimer);
-                dropping = true;
+                Hookshot();
+                print("Hookshot!");
             }
-            //print("Jump!");
-            //transform.position = Vector2.MoveTowards(transform.position, transform.position + Vector3.up * Input.GetAxis("Vertical"), 10 * Time.deltaTime);
-            
-            //StartCoroutine("JumpOff");
-            //print("Jumped!");
-
         }
+
+        if (!isAiming)
+        {
+
+            // DropDown Interaction
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (!dropping)
+                {
+                    dropping = true;
+                }
+            }
+        }
+
+    }
+
+    void Hookshot()
+    {
+
+    }
+
+    void DropDownCheck()
+    {
+        if (dropping)
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+
+            dropElapsed += Time.deltaTime;
+            if (dropElapsed > 0.4f)
+            {
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                dropElapsed = 0f;
+                dropping = false;
+            }
+        }
+    }
+
+    float LerpSpeed()
+    {
+        if (!isAiming)
+        {
+            if (Input.GetButton("Horizontal"))
+            {
+                // the time elapsed since the timer was started
+                float timeElapsed = Time.time - startTimer;
+                // the percent of time elapsed in relation to the speed change interval
+                float percent = timeElapsed / changeSpeedInterval;
+                if (percent > 1)
+                {
+                    ResetTimer();
+                }
+                // calculate the current speed, using percent and the animation curve
+                return speed = Mathf.Lerp(initialSpeed, targetSpeed, animationCurve.Evaluate(percent));
+            }
+            if (Input.GetButton("Horizontal") && !isGrounded)
+            {
+                startTimer = 10f;
+            }
+            if (Input.GetButtonUp("Horizontal") && speed != 0f)
+            {
+                return speed = 0f;
+            }
+        }
+        return speed = Mathf.Lerp(speed, 0f, animationCurve.Evaluate(percent));
+    }
+
+    void ResetTimer()
+    {
+        startTimer = Time.time;
+    }
+
+    void AnimateSprite()
+    {
         // Flip sprite
         if (horizontalInput < 0 && isFacingRight)
         {
@@ -107,57 +182,7 @@ public class InputMovement : MonoBehaviour
         // Set Animator variables
         animatorComponent.SetFloat("horizontalSpeed", Mathf.Abs(horizontalInput));
         animatorComponent.SetBool("isGrounded", isGrounded);
+        animatorComponent.SetBool("isAiming", isAiming);
 
     }
-
-    void DropDownCheck()
-    {
-        if (dropping)
-        {
-            print("Drop!");
-            gameObject.GetComponent<Collider2D>().enabled = false;
-
-            dropElapsed += Time.deltaTime;
-            if (dropElapsed > 0.4f)
-            {
-                gameObject.GetComponent<Collider2D>().enabled = true;
-                dropElapsed = 0f;
-                dropping = false;
-            }
-        }
-    }
-
-
-    float LerpSpeed()
-    {
-
-        if (Input.GetButton("Horizontal"))
-        {
-            
-            // the time elapsed since the timer was started
-            float timeElapsed = Time.time - startTimer;
-            // the percent of time elapsed in relation to the speed change interval
-            float percent = timeElapsed / changeSpeedInterval;
-            if (percent > 1)
-            {
-                ResetTimer();
-            }
-            // calculate the current speed, using percent and the animation curve
-            return speed = Mathf.Lerp(initialSpeed, targetSpeed, animationCurve.Evaluate(percent));
-        }
-
-        if (Input.GetButtonUp("Horizontal") && speed != 0f)
-        {
-            return speed = 0f;
-        }
-
-        return speed = Mathf.Lerp(speed, 0f , animationCurve.Evaluate(percent));
-
-    }
-
-    void ResetTimer()
-    {
-        startTimer = Time.time;
-    }
-
 }
